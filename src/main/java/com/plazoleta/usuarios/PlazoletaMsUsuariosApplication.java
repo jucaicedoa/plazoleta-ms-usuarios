fdc01fcb3faebcc3b4667a7b00c6481ccb4bd313
jucaicedoa <jucaicedoa@unal.edu.co>
2026-01-25 09:43:17 -0500

    first commit

---
+++ src/main/java/com/plazoleta/usuarios/PlazoletaMsUsuariosApplication.java
@@ -0,0 +1,13 @@
+package com.plazoleta.usuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlazoletaMsUsuariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlazoletaMsUsuariosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/plazoleta/usuarios/PlazoletaMsUsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plazoleta.usuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlazoletaMsUsuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
